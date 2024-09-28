8b57318ded49adf41ec58d41d93450ee122fce0f
rookxd <muzafferpro323232@gmail.com>
2024-09-28 23:59:25 +0300

    Update config.lua

--- rook-stash/config.lua
+++ rook-stash/config.lua
@@ -1,19 +1,10 @@
 Config = {}
 
 Config.Inventory = 'ox' -- 'qb'  'ox'
-Config.Target = 'interact' -- 'target'  'interact'  'drawtext'
+Config.Target = 'interact' -- 'target'  'interact'
 
 Config.Depo = {
     [1] = {
-        {loc = vector3(-1692.6, -1151.68, 13.04), label = "Depo", distance = 2.0, depo = "test-Depo2", job = "police", kilo = 1000000, slot = 100, id = 'sade2'}
-    },
-    [2] = {
-        {loc = vector3(-1700.46, -1144.64, 13.11), label = "Depo", distance = 0.6, depo = "test-Depo3", job = "ambulance", kilo = 1000000, slot = 100, id = 'sade'}
-    },
-    [3] = {
-        {loc = vector3(-1697.08, -1147.04, 13.09), label = "Depo", distance = 0.6, depo = "test-Depo4", job = "ambulance", kilo = 1000000, slot = 100, id = 'sade3'}
-    },
-    [4] = {
-        {loc = vector3(-1707.7, -1135.0, 13.15), label = "Depo", distance = 0.6, depo = "test-Depo5", job = "ambulance", kilo = 1000000, slot = 100, id = 'sade4'}
+        {loc = vector3(-1692.6, -1151.68, 13.04), label = "Depo", distance = 2.0, depo = "test-Depo", job = "police", kilo = 1000000, slot = 100, id = 'depo'}
     },
 }
