25bde4d32da9df0fdce0cf6ff6fc7e0439c99786
rookxd <muzafferpro323232@gmail.com>
2024-09-29 19:57:06 +0300

    Update config.lua

--- rook-stash/config.lua
+++ rook-stash/config.lua
@@ -8,3 +8,15 @@ Config.Depo = {
         {loc = vector3(-1692.6, -1151.68, 13.04), label = "Depo", distance = 2.0, depo = "test-Depo", job = "police", kilo = 1000000, slot = 100, id = 'depo'}
     },
 }
+
+Config.KisiselSlot = 1000 
+Config.KisiselKilo = 1000000 
+
+Config.KisiselDepo = {
+    [1] = {
+        {loc = vector3(-1683.16, -1158.72, 13.02), label = "Kişisel Depo", distance = 2.0, id = 'kdepo'}
+    },
+    [2] = {
+        {loc = vector3(-1672.95, -1165.23, 13.02), label = "Kişisel Depo", distance = 2.0, id = 'kdepo'}
+    },
+}
